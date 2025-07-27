3856b7322a694201363c625207b3a321f54a2d72
mohan-cao <mohancao@yahoo.com.au>
2025-07-27 06:01:52 +0000

    fix: missing libs causing problems

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 # Source docker-entrypoint.sh:
@@ -10,23 +9,14 @@ if [ "$1" = 'freeswitch' ]; then
     if [ ! -f "/etc/freeswitch/freeswitch.xml" ]; then
         mkdir -p /etc/freeswitch
         cp -varf /usr/share/freeswitch/conf/vanilla/* /etc/freeswitch/
-	rm -rf /usr/share/freeswitch/conf
-	echo "Copying vanilla config for freeswitch xml"
     fi
-    
-    chown -R freeswitch:freeswitch /usr/local/freeswitch
-
     if [ -d /docker-entrypoint.d ]; then
         for f in /docker-entrypoint.d/*.sh; do
             [ -f "$f" ] && . "$f"
         done
     fi
 
-    exec gosu freeswitch /usr/bin/freeswitch -u freeswitch -g freeswitch \
-        -conf /etc/freeswitch \
-	-db /usr/local/freeswitch/db \
-        -log /usr/local/freeswitch/log \
-	-nonat -c -nf
+    /usr/bin/freeswitch -nonat -c -nf
 fi
 
 exec "$@"
